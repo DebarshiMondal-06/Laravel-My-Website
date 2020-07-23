4deeba04b85e87aa06ef44a51d6e19de11d237b7
Debarshi <debopiku1122@gmail.com>
2020-07-23 20:41:11 +0530

    Category index page setup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,30 +7,45 @@ use App\Post;
 use App\Category;
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+   /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+   // public function __construct()
+   // {
+   //     $this->middleware('auth');
+   // }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+   /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
 
-     public function index() {
-        $category = Category::all();
-        $posts = Post::orderby('created_at','DESC')->limit(3)->get();
-        return view('Home.index_page',compact('posts','category'));
-     }
+   public function index() {
+      $category = Category::all();
+      $posts = Post::orderby('created_at','DESC')->limit(3)->get();
+      return view('Home.index_page',compact('posts','category'));
+   }
 
-     public function single_category($id) {
-        $single = Post::where('categories_id',$id)->get();
-        return view('Home.categories_page',['single_blog'=>$single]);
-     }
+   public function single_category($id) {
+      $category = Category::where('id',$id)->first();
+      $single = Post::where('categories_id',$id)->orderby('created_at','DESC')->get();
+      $popular_post = Post::where('categories_id',$id)->orderby('created_at','DESC')->take(5)->get();
+      if ($single) {
+         return view('Home.categories_page',
+         [
+            'single_blog'=>$single,
+            'category'=>$category,
+            'popular_post'=>$popular_post
+         ]);
+      }
+
+
+   }
+   // public function popular_post($id) {
+   //    $popular_post = Post::where('categories_id',$id)->orderby('created_at','DESC')->limit(1)->get();
+   //    return view('Home.categories_page',['popular_blog'=>$popular_post]);
+   // }
 }
--- resources/views/Home/categories_page.blade.php
+++ resources/views/Home/categories_page.blade.php
@@ -12,6 +12,7 @@
    <link rel="stylesheet" href="{!! asset('css/theme/style.css') !!}">
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <link href="{!! asset('css/sb-admin-2.min.css') !!}" rel="stylesheet">
 </head>
 
 <body>
@@ -70,46 +71,61 @@
          </div>
       </div>
    </header>
-   <!-- Header part end-->
 
-   <!-- breadcrumb start-->
-   {{-- <section class="breadcrumb breadcrumb_bg align-items-center">
-   <div class="container">
-   <div class="row align-items-center justify-content-between">
-   <div class="col-sm-6">
-   <div class="breadcrumb_tittle text-left">
-   <h2>Category Page</h2>
-</div>
-</div>
-<div class="col-sm-6">
-<div class="breadcrumb_content text-right">
-<p>Home<span>/</span>Category Page</p>
-</div>
-</div>
-</div>
-</div>
-</section> --}}
-<!-- breadcrumb start-->
+   <section class="all_post section_padding">
+      <div class="container" style="margin-top: -8%;">
+         <h1 class="h2 mb-5 text-gray-800"> <a href="" class="font-italic">Category</a>/{{ $category->name }}</h1>
+         <div class="row">
+            <div class="col-md-8">
+               @foreach ($single_blog as $wise)
+                  <div class="card shadow mb-4">
+                     <div class="card-body">
+                        <div class="row">
+                           <div class="col-md-4">
+                              <img style="border-radius: 10px;" height="100%" width="100%" src="{{ asset('public/storage/'.$wise->post_image) }}" alt="">
+                           </div>
+                           <div class="col-md-8">
+                              <h4 class="h3 mt-2 mb-2 p-1">{{ $wise->MainTitle }}</h4>
+                              <div class="post-meta mb-1 p-1">
+                                 <span class="d-block text-info"><li class=" text-dark fas fa-user"></li>&nbsp;{{ $wise->user->name }} &nbsp;&nbsp;&nbsp; <i class="text-dark far fa-clock"></i>&nbsp;{{ $wise->created_at->diffforhumans() }}</span>
+                              </div>
+                              <p class="mb-3 p-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum<a href=""> Read More...</a></p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               @endforeach
+            </div>
+            <div class="col-md-4">
+               <div class="card shadow mb-4">
+                  <div class="card-body">
+                     <h4 class="h4 mt-2 mb-3 p-1 text-uppercase" style="text-decoration: underline;">Popular Post</h4>
+                     @foreach ($popular_post as $popular)
+                        <p class="" style="line-height: 0.8;"><a href="{{ $popular->id }}">{{ $popular->MainTitle }}</a></p>
+                        <div class="post-meta p-1" style="margin-top: -8px;">
+                           <span class="d-block text-info"><li class=" text-dark fas fa-user"></li>&nbsp;{{ $popular->user->name }} &nbsp;&nbsp;&nbsp; <i class="text-dark far fa-clock"></i>&nbsp;{{ $popular->created_at->diffforhumans() }}</span>
+                        </div>
+                        <hr>
+                     @endforeach
+                  </div>
+               </div>
+            </div>
 
-<!-- feature_post start-->
-<section class="all_post section_padding">
-   <div class="container">
 
 
-
-   </div>
-</section>
-<!-- feature_post end-->
+         </div>
+      </section>
+      <!-- feature_post end-->
 
 
 
 
 
-<!-- jquery plugins here-->
-<!-- jquery -->
-<script src="{!! asset('js/themejs/jquery-1.12.1.min.js') !!}"></script>
-<script src="{!! asset('js/themejs/custom.js') !!}"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
+      <!-- jquery plugins here-->
+      <!-- jquery -->
+      <script src="{!! asset('js/themejs/jquery-1.12.1.min.js') !!}"></script>
+      <script src="{!! asset('js/themejs/custom.js') !!}"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+   </body>
 
-</html>
+   </html>
